419eff41cdb477fd263c434a0c100fafb72a4bf9
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-11 23:15:13 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+
+  attr_accessor :title, :author
+  
+  def initialize(title)
+    @title = title
+    # @author = author
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+  # def self.all 
+  #   @@all
+  # end 
+end
